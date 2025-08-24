05254465f0479a319a3abe29d59ba1b0a20487a6
𝒜𝓇𝒾𝑒𝐼𝓃𝓋𝒶𝒹𝑒𝓇 <darienayman3@gmail.com>
2025-08-24 22:15:00 +0300

    Add files via upload

---
+++ BMI_Calc.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title("BMI Calculator")
+st.image("bmi.PNG")
+weight = st.number_input("Enter your weight in kgs :")
+height = st.number_input("Enter your height in meter :")
+bmi = weight / (height**2)
+if st.button("Calculate") :
+    st.write(f"Your BMI : {bmi : .2f}")
+if bmi < 18.5 :
+   st.success("Category : Underweight")
+elif bmi >= 18.5 and bmi <= 24.9 :
+    st.success("Category : Normal Weight")
+elif bmi>=25 and bmi<=29.9 :
+    st.success("Category : Class 1 Obesity")
+elif bmi>=30  and bmi <= 39.9 :
+    st.success("Category : Class 2 Obesity")
+else :
+    st.success("Category : Class 3 Obesity")
